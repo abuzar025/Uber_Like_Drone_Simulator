30f67161c0ecd3719a5950e75c2a3831903cfd19
anirudh ganesh <ganes099@umn.edu>
2023-04-13 14:08:00 -0500

    Created and Documented RechargeStationFactory.h

---
+++ libs/transit/include/RechargeStationFactory.h
@@ -0,0 +1,26 @@
+#ifndef RECHARGE_STATION_FACTORY_H_
+#define RECHARGE_STATION_FACTORY_H_
+
+#include <vector>
+
+#include "IEntityFactory.h"
+#include "RechargeStation.h"
+
+/**
+ *@brief Recharge Station Factory to produce Recharge Stations.
+ **/
+class RechargeStationFactory : IEntityFactory {
+    public:
+        /**
+        * @brief Destructor for IEntityFactory class.
+        **/
+        virtual ~RechargeStationFactory() {}
+
+        /**
+        * @brief Creates entity using the given JSON object, if possible.
+        * @param entity - JsonObject to be used to create the new entity.
+        * @return Entity that was created if it was created successfully, or a
+        *nullpointer if creation failed.
+        **/
+        IEntity* CreateEntity(JsonObject& entity);
+}
