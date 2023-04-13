8d4e22c3f3b7ae0aac02a5ac9ded3a9bd7c6679e
Muhammad Abuzar <abuza006@umn.edu>
2023-04-13 13:53:39 -0500

    BettteryDecorator.h

---
+++ libs/transit/include/BatteryDecorator.h
@@ -0,0 +1,22 @@
+#ifndef BATTERY_DECORATOR_H_
+#define BATTERY_DECORATOR_H_
+
+#include "IEntityDecorator.h"
+
+class BatteryDecorator: public IEntityDecorator {
+  public:
+    /**
+     * @brief Constructor that assigns a unique ID to the battery.
+     */
+    BatteryDecorator(IEntity* wrapped): IEntityDecorator(wrapped) {}
+
+     /**
+     * @brief What to override to modify the baheavior of the surrounding IEntity
+     * @param dt The time step of the update.
+     * @param scheduler The list of all entities in the system.
+     */
+    virtual void OverridedUpdate(double dt, std::vector<IEntity*> scheduler);
+
+};
+
+#endif
