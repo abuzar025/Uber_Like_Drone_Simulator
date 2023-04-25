5fdaed4cda2ea748ea34081ab5be1b9bb1fdb6ad
Muhammad Abuzar <abuza006@umn.edu>
2023-04-25 15:34:32 -0500

    Battery percent 100

--- libs/transit/src/DroneFactory.cc
+++ libs/transit/src/DroneFactory.cc
@@ -5,7 +5,7 @@ IEntity* DroneFactory::CreateEntity(JsonObject& entity) {
   std::string type = entity["type"];
   if (type.compare("drone") == 0) {
     std::cout << "Drone Created" << std::endl;
-    return new BatteryDecorator(new Drone(entity), 30, 1);
+    return new BatteryDecorator(new Drone(entity), 100, 1);
   }
   return nullptr;
 }
