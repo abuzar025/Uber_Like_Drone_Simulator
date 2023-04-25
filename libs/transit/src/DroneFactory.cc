6b67e1666b3f74a6211e6feb60c5c1b19a3e4031
Muhammad Abuzar <abuza006@umn.edu>
2023-04-25 15:41:40 -0500

    battery level to 25

--- libs/transit/src/DroneFactory.cc
+++ libs/transit/src/DroneFactory.cc
@@ -5,8 +5,7 @@ IEntity* DroneFactory::CreateEntity(JsonObject& entity) {
   std::string type = entity["type"];
   if (type.compare("drone") == 0) {
     std::cout << "Drone Created" << std::endl;
-    return new BatteryDecorator(new Drone(entity), 25
-    , 1);
+    return new BatteryDecorator(new Drone(entity), 25, 1);
   }
   return nullptr;
 }
